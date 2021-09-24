9c2562c56ac3b9bf73ede6a16b12543048cca24f
sebastianbarreram <sbarrera.96@hotmail.com>
2021-09-24 15:56:56 -0500

    Ejemplo

--- Proyecto Vue/proyecto/src/views/Servicios.vue
+++ Proyecto Vue/proyecto/src/views/Servicios.vue
@@ -1,19 +1,19 @@
 <template>
-  <div >
-      <inicio-sesion />
+  <div>
+    <Header />
+    <Header />
+    <inicio-sesion />
   </div>
 </template>
 
 <script>
-import Header from '../components/Header.vue'
-import InicioSesion from '../components/InicioSesion.vue'
+import Header from "../components/Header.vue";
+import InicioSesion from "../components/InicioSesion.vue";
 
 export default {
-  components: { Header, InicioSesion,  },
-
-}
+  components: { Header, InicioSesion },
+};
 </script>
 
 <style>
-
 </style>
