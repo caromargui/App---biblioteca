e82b66917fe9542b988f277bd41bcff0490bc63f
caromargui <68072978+caromargui@users.noreply.github.com>
2021-09-27 14:59:04 -0400

    pie de pag

--- Proyecto Vue/proyecto/src/components/piedepag.vue
+++ Proyecto Vue/proyecto/src/components/piedepag.vue
@@ -1,10 +1,38 @@
 <template>
   
+      <div> <div class="seccion-pie">
+       
+        <article>
+        <h2>Motivación de la página: </h2>
+        <font size=1><p>Hemos pensado en una página web dirigida a las bibliotecas comunitarias en 
+            la que se tenga un usuario y contraseña para acceder a los <br> cursos y servicios que se ofrezcan
+             por ejemplo un 
+            servicio podría ser asesoría financiera a personas en cuanto a prestamos, nos motiva <br> pensar en una página que ayude a las juntas de acción comunales y la 
+            disponibilidad de la información, para personas que lo necesiten.</p></font>
+            <font size=2><p>Enlace al repositorio:</font> <br>
+                <font size=1>"https://github.com/caromargui/App---biblioteca"]</font>
+       
+    </article>
+    <article>
+        <h2>Contactos</h2>
+        <font size=2><p>Sebastian Barrera (Scrum Master): </font> 
+            <font size=1>sbarrera.96@hotmail.com</font>  <br>
+            <font size=2>Carolina Cova (Product owner):</font> 
+            <font size=1>caromargui@gmail.com</font> <br>
+            <font size=2> Sebastian Rentería (Development Team): </font>
+             <font size=1>sren97@gmail.com</font> <br>
+             <font size=2>Diego Guisao (Development Team):</font>
+             <font size=1>diego.alexander.guisao@gmail.com</font>  <br>
+             <font size=2> Santiago Puello (Development Team):</font>
+              <font size=1>sepuellov@unal.edu.co</font>  <br>
+     </div>
+
+
 </template>
 
 <script>
 export default {
-
+    name = 'Piedepag'
 }
 </script>
 
