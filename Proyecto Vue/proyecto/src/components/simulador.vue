7b350c4b66728dc29f8826ca37dd5765ea5ce810
caromargui <68072978+caromargui@users.noreply.github.com>
2021-09-30 15:32:49 -0400

    Simulador con heade y pie de pag

---
+++ Proyecto Vue/proyecto/src/components/simulador.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Header />  
+        <div class="card" style="width: 18rem;">
+       <img src="..." class="card-img-top" alt="...">
+       <div class="card-body">
+       <h5 class="card-title">Card title</h5>
+       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+    </div>
+
+   <Piedepag />
+  </div>
+</template>
+
+<script>
+import Header from "./Header.vue";
+import Piedepag from './components/Piedepag.vue';
+export default {
+  components: { Header, Piedepag },
+};
+</script>
+
+<style>
+
+</style>
