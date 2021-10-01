f90aa47fc8b19b8235dc3140265131428d59ad9e
caromargui <68072978+caromargui@users.noreply.github.com>
2021-09-30 20:29:06 -0400

    intentando que esto funcione

---
+++ Proyecto Vue/proyecto/src/views/Simulador.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="simulador">
+    <Header />
+    <h1>Este es la vista dlesimulador</h1>
+    <Simulador />
+    <Piedepag />
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue';
+import Piedepag from '../components/Piedepag.vue';
+import Simulador from './components/simulador.vue';
+
+// @ is an alias to /src
+
+export default {
+  name: "Simulador",
+  components: {
+    Piedepag,
+    Header,
+    Simulador
+  },
+};
+</script>
