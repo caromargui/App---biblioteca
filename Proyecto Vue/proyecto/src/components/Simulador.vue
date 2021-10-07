a3079afc3289aac64f2e894d6fab2ac061b44939
caromargui <68072978+caromargui@users.noreply.github.com>
2021-10-06 22:45:01 -0400

    Ya esta mejor el simulador, tengo que corregir la formula para buscar bien la formula y mejorar la tarjeta de al lado

--- Proyecto Vue/proyecto/src/components/Simulador.vue
+++ Proyecto Vue/proyecto/src/components/Simulador.vue
@@ -1,16 +1,51 @@
 <template>
-  <div>
-    <h1>Simulador de credito</h1>
+  <v-container>
+    <v-row >
+      <v-col class="text-center">
+        <h1>Simulador de crédito</h1>
+    </v-col>
+    </v-row>
+    <v-row>
+      <v-col class="text-center">
+        <v-text-field
+          type="number"
+          label="Valor del préstamo"
+          :rules="rules1"
+          hide-details="auto"
+          v-model="valorprestamos"
+        ></v-text-field>
+        <v-text-field
+          type="number"
+          label="Cantidad de cuotas"
+          :rules="rules2"
+          hide-details="auto"
+          v-model="cuotas"
+        ></v-text-field>
+       <input type="number" v-model="cuotas"/>
+       <br/>
+      </v-col>
+      <v-col>
+        <v-card
+          elevation="2"
+          outlined
+          shaped
+        ></v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col class="text-center">
+      <v-btn
+          color="primary"
+          elevation="2"
+          rounded
+          medium
+          @click="cuotafija"
+      >Simular </v-btn>
+      </v-col>
+    </v-row>
     <br/>
-    <input type="number" v-model="valorprestamos"/>
-    <br/>
-    <input type="number" v-model="cuotas"/>
-    <br/>
-    <button @click="cuotafija()">Simular</button>
-    <br />
     {{valorcuota}}
-  </div>
-
+  </v-container>
 </template>
 
 <script>
@@ -21,12 +56,19 @@ export default {
         valorprestamo:null,
         cuotas:null,
         valorcuota:"",
+        rules1:[
+          value => !!value||'Valor obligatorio',
+          value => (value && value >= 1000000)||'Valor obligatorio mayor a $1.000.000'
+        ],
+        rules2:[
+          value => !!value||'Cantidad de cuotas es un valor obligatorio',
+          value => (value && value >= 12 && value <=120)||'La cantidas de cuotas debe ser mayor mayor a 12 y menor a 120'
+        ]
       };
   },
-  methods: {
-    cuotafija(){
+  methods: { 
+      cuotafija(){
       this.valorcuota=((parseFloat (this.valorprestamo))*(0,796575374*((1+0,796575374)^(parseFloat (this.cuotas)))))/(((1+0,796575374)^(parseFloat(this.cuotas)))-1);
-
     },
 
   },
